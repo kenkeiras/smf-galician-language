0b050f40c3a8c4fe3250a1d6a039b913a151f373
kenkeiras <kenkeiras@gmail.com>
2013-07-31 22:55:53 +0200

    Traducción da xestión de correos

--- ManageMail.galician-utf8.php
+++ ManageMail.galician-utf8.php
@@ -2,51 +2,51 @@
 // Version: 2.0; ManageMail
 
 // Important! Before editing these language files please read the text at the top of index.english.php.
-$txt['mailqueue_desc'] = 'From this page you can configure your mail settings, as well as viewing and administrating the current mail queue if it is enabled.';
+$txt['mailqueue_desc'] = 'Dende esta páxina podes configurar os axustes de correo, así coma ver e administrar a cola actual de correo se está activada.';
 
-$txt['mail_type'] = 'Mail type';
-$txt['mail_type_default'] = '(PHP default)';
-$txt['smtp_host'] = 'SMTP server';
-$txt['smtp_port'] = 'SMTP port';
-$txt['smtp_username'] = 'SMTP username';
-$txt['smtp_password'] = 'SMTP password';
+$txt['mail_type'] = 'Tipo de Correo';
+$txt['mail_type_default'] = '(PHP por defecto)';
+$txt['smtp_host'] = 'Servidor SMTP';
+$txt['smtp_port'] = 'Porto SMTP';
+$txt['smtp_username'] = 'Nome de usuario de SMTP';
+$txt['smtp_password'] = 'Contrasinal SMTP';
 
-$txt['mail_queue'] = 'Enable Mail Queue';
-$txt['mail_limit'] = 'Maximum emails to send per minute';
-$txt['mail_limit_desc'] = '(Set to 0 to disable)';
-$txt['mail_quantity'] = 'Maximum amount of emails to send per page load';
+$txt['mail_queue'] = 'Activar cola de correo';
+$txt['mail_limit'] = 'Emails máximos por minuto';
+$txt['mail_limit_desc'] = '(0 para desactivar)';
+$txt['mail_quantity'] = 'Cantidade máxima de emails para enviar por carga de páxina';
 
-$txt['mailqueue_stats'] = 'Mail Queue Statistics';
-$txt['mailqueue_oldest'] = 'Oldest Mail';
+$txt['mailqueue_stats'] = 'Estatísticas da cola de correo';
+$txt['mailqueue_oldest'] = 'Correo máis antigo';
 $txt['mailqueue_oldest_not_available'] = 'N/A';
-$txt['mailqueue_size'] = 'Queue Length';
+$txt['mailqueue_size'] = 'Lonxitude da cola';
 
-$txt['mailqueue_age'] = 'Age';
-$txt['mailqueue_priority'] = 'Priority';
-$txt['mailqueue_recipient'] = 'Recipient';
-$txt['mailqueue_subject'] = 'Subject';
-$txt['mailqueue_clear_list'] = 'Send Mail Queue Now';
-$txt['mailqueue_no_items'] = 'The mail queue is currently empty';
+$txt['mailqueue_age'] = 'Idade';
+$txt['mailqueue_priority'] = 'Prioridade';
+$txt['mailqueue_recipient'] = 'Receptor';
+$txt['mailqueue_subject'] = 'Asunto';
+$txt['mailqueue_clear_list'] = 'Enviar cola de correo agora';
+$txt['mailqueue_no_items'] = 'A cola de correo está actualmente vacía';
 // Do not use numeric entities in below string.
-$txt['mailqueue_clear_list_warning'] = 'Are you sure you wish to send the whole mail queue now? This will override any limits you have set.';
+$txt['mailqueue_clear_list_warning'] = 'Está seguro de que desexa enviar toda a cola de correo agora? Isto ignorará calqueira límite que haia axustado.';
 
-$txt['mq_day'] = '%1.1f Day';
-$txt['mq_days'] = '%1.1f Days';
-$txt['mq_hour'] = '%1.1f Hour';
-$txt['mq_hours'] = '%1.1f Hours';
-$txt['mq_minute'] = '%1$d Minute';
-$txt['mq_minutes'] = '%1$d Minutes';
-$txt['mq_second'] = '%1$d Second';
-$txt['mq_seconds'] = '%1$d Seconds';
+$txt['mq_day'] = '%1.1f Día';
+$txt['mq_days'] = '%1.1f Días';
+$txt['mq_hour'] = '%1.1f Hora';
+$txt['mq_hours'] = '%1.1f Horas';
+$txt['mq_minute'] = '%1$d Minuto';
+$txt['mq_minutes'] = '%1$d Minutos';
+$txt['mq_second'] = '%1$d Segundo';
+$txt['mq_seconds'] = '%1$d Segundos';
 
-$txt['mq_mpriority_5'] = 'Very Low';
-$txt['mq_mpriority_4'] = 'Low';
+$txt['mq_mpriority_5'] = 'Moi baixa';
+$txt['mq_mpriority_4'] = 'Baixa';
 $txt['mq_mpriority_3'] = 'Normal';
-$txt['mq_mpriority_2'] = 'High';
-$txt['mq_mpriority_1'] = 'Very High';
+$txt['mq_mpriority_2'] = 'Alta';
+$txt['mq_mpriority_1'] = 'Moi alta';
 
-$txt['birthday_email'] = 'Birthday Message to use';
-$txt['birthday_body'] = 'Email Body';
-$txt['birthday_subject'] = 'Email Subject';
+$txt['birthday_email'] = 'Mesaxe de aniversario para usar';
+$txt['birthday_body'] = 'Corpo do Email';
+$txt['birthday_subject'] = 'Asunto do Email';
 
 ?>
