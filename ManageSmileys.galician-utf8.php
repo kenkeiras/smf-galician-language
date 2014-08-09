64cf97cd9a70f1587c1e9b3dc5c7ad2e6a1ab554
kenkeiras <kenkeiras@gmail.com>
2014-08-09 18:03:29 +0200

    Tradución de ManageSmileys

--- ManageSmileys.galician-utf8.php
+++ ManageSmileys.galician-utf8.php
@@ -5,23 +5,23 @@
 $txt['smiley_sets_save'] = 'Gardar Cambios';
 $txt['smiley_sets_add'] = 'Nova coleccion de smileys';
 $txt['smiley_sets_delete'] = 'Borrar seleccionados';
-$txt['smiley_sets_confirm'] = '&#191;Estas seguro que desexas eliminar esta coleccion de smileys?\\n\\Nnota: Isto non eliminase as imagenes, só a opcion de los selecciona.';
+$txt['smiley_sets_confirm'] = '&#191;Estás seguro que desexas eliminar esta colección de smileys?\\n\\Nnota: Isto non as imaxes, só a opcion de as seleccionar.';
 
-$txt['setting_smiley_sets_default'] = 'Default Smiley Set';
-$txt['setting_smiley_sets_enable'] = 'Enable smiley set selection by members';
-$txt['setting_smiley_enable'] = 'Enable customized smileys';
-$txt['setting_smileys_url'] = 'Base URL to all smiley sets';
-$txt['setting_smileys_dir'] = 'Absolute path to all smiley sets';
-$txt['setting_messageIcons_enable'] = 'Enable customized message icons';
-$txt['setting_messageIcons_enable_note'] = '(otherwise, the default message icons will be used.)';
-$txt['groups_manage_smileys'] = 'Groups allowed to manage smileys and message icons';
+$txt['setting_smiley_sets_default'] = 'Paquete por defecto de Smileys';
+$txt['setting_smiley_sets_enable'] = 'Permitir a selección de paquete de smileys polos membros';
+$txt['setting_smiley_enable'] = 'Activar smileys personalizados';
+$txt['setting_smileys_url'] = 'URL base de tódolos paquetes de smileys';
+$txt['setting_smileys_dir'] = 'Camiño absoluto a tódolos paquetes de smileys';
+$txt['setting_messageIcons_enable'] = 'Activar iconos de mensaxe personalizados';
+$txt['setting_messageIcons_enable_note'] = '(de outro xeito, os iconos de mensaxe por defecto serán usados.)';
+$txt['groups_manage_smileys'] = 'Grupos que poden manexar smileys e iconos de mensaxes';
 
-$txt['smiley_sets_name'] = 'Nome da coleccion';
+$txt['smiley_sets_name'] = 'Nome da colección';
 $txt['smiley_sets_url'] = 'URL';
-$txt['smiley_sets_default'] = 'Default';
+$txt['smiley_sets_default'] = 'Colección por defecto';
 
 $txt['smiley_sets_latest'] = 'Smileys do Momento';
-$txt['smiley_sets_latest_fetch'] = 'Cargando a informacion dos smileys dende simplemachines.org...';
+$txt['smiley_sets_latest_fetch'] = 'Cargando a información dos smileys dende simplemachines.org...';
 
 $txt['smileys_add_method'] = 'Fonte da Imaxe';
 $txt['smileys_add_existing'] = 'Usar arquivo existente';
@@ -30,58 +30,58 @@ $txt['smileys_add_upload_choose'] = 'Arquivo a subir';
 $txt['smileys_add_upload_choose_desc'] = 'Imaxe para ser usada por tódalas coleccións de smileys.';
 $txt['smileys_add_upload_all'] = 'Usa-la mesma imaxe para tódalas coleccións';
 $txt['smileys_add_upload_for1'] = 'Imaxe para a';
-$txt['smileys_add_upload_for2'] = 'coleccion';
+$txt['smileys_add_upload_for2'] = 'colección';
 
-$txt['smileys_enable_note'] = '(o contrario, os smileys por defecto seran utilizados.)';
-$txt['smileys_code'] = 'Codigo';
+$txt['smileys_enable_note'] = '(de outro xeito, os smileys por defecto seran utilizados.)';
+$txt['smileys_code'] = 'Código';
 $txt['smileys_filename'] = 'Nome do Arquivo';
-$txt['smileys_description'] = 'Tooltip ou descripcion';
+$txt['smileys_description'] = 'Tooltip ou descripción';
 $txt['smileys_remove'] = 'Eliminar';
 $txt['smileys_save'] = 'Gardar cambios';
-$txt['smileys_delete'] = 'Delete Smiley';
+$txt['smileys_delete'] = 'Eliminar Smiley';
 // Don't use entities in the below string.
-$txt['smileys_delete_confirm'] = 'Are you sure you wish to delete this smiley?';
+$txt['smileys_delete_confirm'] = 'Está seguro de que desexa eliminar este smiley?';
 $txt['smileys_with_selected'] = 'Cos seleccionados';
-$txt['smileys_make_hidden'] = 'Los face ocultos';
-$txt['smileys_show_on_post'] = 'Los amosa na forma de publicar mensaxe';
-$txt['smileys_show_on_popup'] = 'Lo amosa no Popup';
+$txt['smileys_make_hidden'] = 'Ocultalos';
+$txt['smileys_show_on_post'] = 'Amosalos no formulario de publicar mensaxe';
+$txt['smileys_show_on_popup'] = 'Amosalos no Popup';
 
-$txt['smiley_settings_explain'] = 'Estas opcioes permítente cambia-la coleccion de smiley de default, permitiendole ós usuarios selecciona-los seus propios smileys, e configurar datos da ruta e a configuracion.';
-$txt['smiley_editsets_explain'] = 'As coleccións de smileys son grupos de smileys que os teus usuarios poden elixir.  Por exemplo, podes ter smileys amarelos e vermellos.<br /> Aqui podes cambia-lo nome e a ubicacion de cada coleccion de smileys.';
-$txt['smiley_editsmileys_explain'] = 'Cambia os teus smileys aqui facendo <i>click</i> no smiley que desexas modificar. &iexcl;Lembranza que todos eses smileys deben existir en tódalas coleccións ou algúns smileys non se amosasen!  &iexcl;Non esquezas gardar xa que remates de editar!';
-$txt['smiley_setorder_explain'] = 'Cambia o orde dos smileys aqui.';
-$txt['smiley_addsmiley_explain'] = 'Aqui podes agregar un novo smiley - xa sexa dun arquivo existente ou subindo novos.';
+$txt['smiley_settings_explain'] = 'Estas opcións permítente cambia-la colección de smiley por defecto, permitiendolle ós usuarios selecciona-los seus propios smileys, e configurar datos da ruta e a configuración.';
+$txt['smiley_editsets_explain'] = 'As coleccións de smileys son grupos de smileys que os teus usuarios poden elixir.  Por exemplo, pódes ter smileys amarelos e vermellos.<br /> Aquí podes cambia-lo nome e a ubicación de cada colección de smileys.';
+$txt['smiley_editsmileys_explain'] = 'Cambia os teus smileys aquí facendo <i>click</i> no smiley que desexas modificar. &iexcl;Lembra que todos eses smileys deben existir en tódalas coleccións ou algúns smileys non se amosarán!  &iexcl;Non esquezas gardar cando remates de editar!';
+$txt['smiley_setorder_explain'] = 'Cambia a orde dos smileys aquí.';
+$txt['smiley_addsmiley_explain'] = 'Aquí podes agregar un novo smiley - xa sexa dun arquivo existente ou subindo novos.';
 
-$txt['smiley_set_select_default'] = 'Conxunto de smileys de default';
+$txt['smiley_set_select_default'] = 'Conxunto de smileys por defecto';
 $txt['smiley_set_new'] = 'Crear un novo conxunto de smileys';
-$txt['smiley_set_modify_existing'] = 'Modify existing smiley set';
+$txt['smiley_set_modify_existing'] = 'Modificar o conxunto de smileys existente';
 $txt['smiley_set_modify'] = 'Modificar';
 $txt['smiley_set_import_directory'] = 'Importa-los smileys que xa se atopan neste directorio';
 $txt['smiley_set_import_single'] = 'Hai un smiley nesta coleccion que todavia non foi importado. Fai <i>click</i>';
 $txt['smiley_set_import_multiple'] = 'Hai %d smileys neste directorio que aínda non foron importados. Fai <i>click</i>';
-$txt['smiley_set_to_import_single'] = 'para lo importa agora.';
-$txt['smiley_set_to_import_multiple'] = 'para los importa agora.';
+$txt['smiley_set_to_import_single'] = 'para importalo agora.';
+$txt['smiley_set_to_import_multiple'] = 'para importalos agora.';
 
-$txt['smileys_location'] = 'Ubicacion';
-$txt['smileys_location_form'] = 'Forma do mensaxe';
+$txt['smileys_location'] = 'Ubicación';
+$txt['smileys_location_form'] = 'Formulario da mensaxe';
 $txt['smileys_location_hidden'] = 'Oculto';
 $txt['smileys_location_popup'] = 'Popup';
 $txt['smileys_modify'] = 'modificar';
 $txt['smileys_not_found_in_set'] = 'Smiley non atopado nas coleccións';
-$txt['smileys_default_description'] = '(Introduce unha descripcion)';
+$txt['smileys_default_description'] = '(Introduce unha descripción)';
 $txt['smiley_new'] = 'Agregar novo smiley';
 $txt['smiley_modify_existing'] = 'Modificar smiley';
 $txt['smiley_preview'] = 'Previsualizar';
-$txt['smiley_preview_using'] = 'usando a coleccion de smileys';
-$txt['smileys_confirm'] = '&#191;Est&#225;s seguro que queres eliminar estes smileys?\\n\\Nnota: Isto non borrase as im&#225;genes, solamente as opcións.';
-$txt['smileys_location_form_description'] = 'Estes smileys apareceran sobre o area de texto, ó publicar un novo mensaxe no foro ou un novo mensaxe persoal.';
-$txt['smileys_location_popup_description'] = 'Estes smileys apareceran nun popup, que é amosado despues de que un usuario fixo <i>click</i> en \'[mais]\'';
+$txt['smiley_preview_using'] = 'usando a colección de smileys';
+$txt['smileys_confirm'] = '&#191;Est&#225;s seguro que queres eliminar estes smileys?\\n\\Nnota: Isto non borra as im&#225;xes, soamente as opcións.';
+$txt['smileys_location_form_description'] = 'Estes smileys aparecerán sobre o area de texto, ó publicar un novo mensaxe no foro ou un novo mensaxe persoal.';
+$txt['smileys_location_popup_description'] = 'Estes smileys apareceran nun popup, que é amosado despues de que un usuario fai <i>click</i> en \'[máis]\'';
 $txt['smileys_move_select_destination'] = 'Seleciona o destino para o smiley';
 $txt['smileys_move_select_smiley'] = 'Seleciona o smiley a mover';
-$txt['smileys_move_here'] = 'Mover smiley a esta ubicacion';
-$txt['smileys_no_entries'] = 'There are currently no smileys configured.';
+$txt['smileys_move_here'] = 'Mover smiley a esta ubicación';
+$txt['smileys_no_entries'] = 'Non hai smileys configurados neste momento.';
 
-$txt['icons_edit_icons_explain'] = 'Aqui podes cambia-los iconas dos mensaxes que estaran dispoñibles no teu foro. Podes agregar, editar, e eliminar iconas, asi como limita-lo seu uso a algúns foros.';
+$txt['icons_edit_icons_explain'] = 'Aquí podes cambia-los iconas dos mensaxes que estaran dispoñibles no teu foro. Podes agregar, editar, e eliminar iconas, así como limita-lo seu uso a algúns foros.';
 $txt['icons_edit_icons_all_boards'] = 'Dispoñible en tódolos foros';
 $txt['icons_board'] = 'Foro';
 $txt['icons_confirm'] = '&#191;Est&#225;s seguro que desexas eliminar eses iconas?\\n\\Nesto só impedir&#225; que se poidan utilizar en novos mensaxes, as im&#225;genes permanecer&#225;n nos xa existentes.';
@@ -90,8 +90,8 @@ $txt['icons_add_new'] = 'Agregar novo icona';
 $txt['icons_edit_icon'] = 'Edita-lo mensaxe do icona';
 $txt['icons_new_icon'] = 'Novo mensaxe do icona';
 $txt['icons_location_first_icon'] = 'Como primeiro icona';
-$txt['icons_location_after'] = 'Despues';
+$txt['icons_location_after'] = 'Despois';
 $txt['icons_filename_all_gif'] = 'Tódolos arquivos deben ser arquivos &quot;gif&quot;';
-$txt['icons_no_entries'] = 'There are currently no message icons configured.';
+$txt['icons_no_entries'] = 'Non hai iconos de mensaxe configurados.';
 
 ?>
